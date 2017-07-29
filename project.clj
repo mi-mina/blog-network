2af778ce50db96344bad1b5e7b549312f48ce5f1
Esperanza Moreno Cruz <hola@mi-mina.com>
2017-07-29 20:04:16 +0200

    blog lists

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject blog_network "0.1.0"
+  :description "Antropoloops blogs network"
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [enlive "1.1.1"]
+                 [ring "1.2.0"]
+                 [net.cgrand/moustache "1.1.0"]])
